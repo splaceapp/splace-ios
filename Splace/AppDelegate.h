98d3d34d4602d578ef06dc831bc6fe150dbbf017
Oleg Agapov <oleg.agapov@gmail.com>
2014-10-09 11:52:54 +0300

    init

---
+++ Splace/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Splace
+//
+//  Created by Oleg Agapov on 10/9/14.
+//  Copyright (c) 2014 Oleg Agapov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Splace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Splace
+//
+//  Created by Oleg Agapov on 10/9/14.
+//  Copyright (c) 2014 Oleg Agapov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
